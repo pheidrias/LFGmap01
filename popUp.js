f7260f74fec26665d207423d67a9467de9bab41b
pheidrias <github.com@pheidrias.info>
2021-05-07 20:33:53 +0000

    Deploying to gh-pages from master @ 22cfb3c489d4b13dda5682e61a47496c25430f00 🚀

--- popUp.js
+++ popUp.js
@@ -1,1 +1,22 @@
-WA.sendChatMessage('Hello world', 'Mr Robot');
+let helloWorldPopup;
+
+// Open the popup when we enter a given zone
+helloWorldPopup = WA.onEnterZone('myZone', () => {
+    WA.openPopup(
+     "popupRectangle", 
+     'Hello world!', 
+     [{
+        label: "Close",
+        className: "primary",
+        callback: (popup) => {
+            // Close the popup when the "Close" button is pressed.
+            popup.close();
+        }
+     }]
+    );
+});
+
+// Close the popup when we leave the zone.
+WA.onLeaveZone('myZone', () => {
+    helloWorldPopup.close();
+});
