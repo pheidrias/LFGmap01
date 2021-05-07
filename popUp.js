22cfb3c489d4b13dda5682e61a47496c25430f00
pheidrias <github.com@pheidrias.info>
2021-05-07 22:32:17 +0200

    04

--- popUp.js
+++ popUp.js
@@ -2,17 +2,19 @@ let helloWorldPopup;
 
 // Open the popup when we enter a given zone
 helloWorldPopup = WA.onEnterZone('myZone', () => {
-    WA.openPopup("popupRectangle", 'Hello world!', [{
+    WA.openPopup(
+     "popupRectangle", 
+     'Hello world!', 
+     [{
         label: "Close",
         className: "primary",
         callback: (popup) => {
             // Close the popup when the "Close" button is pressed.
             popup.close();
         }
-    }]
-    )
-    ;
-}]);
+     }]
+    );
+});
 
 // Close the popup when we leave the zone.
 WA.onLeaveZone('myZone', () => {
