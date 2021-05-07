c071e0e83a0606a8333e23033c9890b4f17c75c2
pheidrias <github.com@pheidrias.info>
2021-05-07 11:48:06 +0200

    04

--- popUp.js
+++ popUp.js
@@ -1,1 +1,13 @@
-WA.sendChatMessage('Hello world', 'Mr Robot');
+let helloWorldPopup;
+
+// Open the popup when we enter a given zone
+helloWorldPopup = WA.onEnterZone('pop1', () => {
+    WA.openPopup("popupRectangle", 'Vorsicht! Der Friedhoft spielt verrückt...kommst Du ins Labor?', [{
+        label: "Close",
+        className: "primary",
+        callback: (popup) => {
+            // Close the popup when the "Close" button is pressed.
+            popup.close();
+        }
+    });
+}]);
