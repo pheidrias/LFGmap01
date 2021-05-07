4a03773df91fb886c3de638e1b6c79339413e49e
pheidrias <github.com@pheidrias.info>
2021-05-07 22:24:28 +0200

    04

--- popUp.js
+++ popUp.js
@@ -9,7 +9,7 @@ helloWorldPopup = WA.onEnterZone('myZone', () => {
             // Close the popup when the "Close" button is pressed.
             popup.close();
         }
-    });
+    })
 }]);
 
 // Close the popup when we leave the zone.
